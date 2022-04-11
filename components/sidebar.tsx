73d4f4b8f3aad37ad4b0c45ce5d3ac000771023f
loyher343 <loyhernandez59@gmail.com>
2022-04-10 22:06:19 -0500

    added logo

---
+++ components/sidebar.tsx
@@ -0,0 +1,37 @@
+import NextImage from 'next/image'
+import {Box, List, ListItem, ListIcon, Divider, Center, LinkBox, LinkOverlay} from '@chakra-ui/layout'
+import {MdHome, MdSearch, MdLibraryMusic, MdPlaylistAdd, MdFavorite} from 'react-icons/md'
+
+const navMenu = [
+    {
+        name: 'Home',
+        icon: MdHome,
+        route: '/',
+    },
+    {
+        name: 'Search',
+        icon: MdSearch,
+        route: '/search',
+    },
+    {
+        name: 'Your Library',
+        icon: MdLibraryMusic,
+        route: '/library',
+    },
+]
+
+const Sidebar = () => {
+    return (
+        <Box width="100%" height="calc(100vh - 100px)" bg="black" paddingX="5px" color="gray">
+            sidebar
+            <Box paddingY="20px">
+                <Box width="120px" marginBottom="20px" paddingX="20px">
+                    <NextImage src="/tracks.svg" height={60} width={120}/>
+                </Box>
+
+            </Box>
+        </Box>
+    )
+}
+
+export default Sidebar
